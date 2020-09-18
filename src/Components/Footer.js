c80888fd0a4aa2b1a8e153b285c0b5ef83edc364
Aritra Roy <aritraroy24@gmail.com>
2020-09-18 23:12:52 +0530

    Visitor Count added with the help of 'www.webfreecounter.com'

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
 
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
+    if (this.props.data) {
+      var networks = this.props.data.social.map(function (network) {
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
     }
@@ -12,25 +12,28 @@ class Footer extends Component {
     return (
       <footer>
 
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">
               {networks}
-           </ul>
+            </ul>
 
-           <ul>
-            <li>Made with ❤️ by Aritra Roy</li>
-           </ul>
+            <ul>
+              <li>Made with ❤️ by Aritra Roy</li>
+            </ul>
 
-           <ul className="copyright">
+            <ul className="copyright">
               {/* <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li> */}
               {/* <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li> */}
-           </ul>
+            </ul>
 
+          </div>
+          <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
+        {/* <!-- Start of WebFreeCounter Code --> */}
+<a href="https://www.webfreecounter.com/" target="_blank"><img src="https://www.webfreecounter.com/hit.php?id=gefnkad&nd=3&style=16" border="0" alt="free counter" /></a>
+{/* <!-- End of WebFreeCounter Code --> */}
+      </footer>
     );
   }
 }
