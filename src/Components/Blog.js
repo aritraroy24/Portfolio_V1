bd26b971e621730251e5080aeeb104eb5102a02f
Aritra Roy <aritraroy24@gmail.com>
2020-12-03 12:34:00 +0530

    Spartan (Part - 2) blog added

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -3,20 +3,22 @@ import React, { Component } from 'react';
 class Blog extends Component {
   render() {
 
-    if(this.props.data){
-      var blogs = this.props.data.blogs.map(function(blogs){
-        var blogImage = 'images/blog/'+blogs.image;
+    if (this.props.data) {
+      var blogs = this.props.data.blogs.map(function (blogs) {
+        var blogImage = 'images/blog/' + blogs.image;
         return <div key={blogs.title} className="columns portfolio-item">
-           <div className="item-wrap">
+          <div className="item-wrap">
             <a target="_blank" href={blogs.url} title={blogs.title}>
-               <img alt={blogs.title} src={blogImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{blogs.title}</h5>
-                     <p>{blogs.category}</p>
-                  </div>
+              <img alt={blogs.title} src={blogImage} />
+              <div className="overlay">
+                <div className="portfolio-item-meta">
+                  <h5>{blogs.title}</h5>
+                  <p>{blogs.category}</p>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
+              </div>
+              <div className="link-icon"><i className="fa fa-link"></i>
+                <div className="date">{blogs.date}</div>
+              </div>
             </a>
           </div>
         </div>
@@ -26,18 +28,18 @@ class Blog extends Component {
     return (
       <section id="portfolio">
 
-      <div className="row">
+        <div className="row">
 
-         <div className="twelve columns collapsed">
+          <div className="twelve columns collapsed">
 
-            <h1>Read My Blogs on Medium</h1>
+            <h1>Read My Latest Blogs on Medium</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {blogs}
+              {blogs}
             </div>
           </div>
-      </div>
-   </section>
+        </div>
+      </section>
     );
   }
 }
