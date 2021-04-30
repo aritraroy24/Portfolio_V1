ee84dc21637f4480ef59342e310257fa7a8fcd91
Aritra Roy <aritraroy24@gmail.com>
2021-05-01 00:18:25 +0530

    BuyCoffee button added and CGPA fixed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,56 +1,58 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  render() {
+   render() {
+      var BuyCoffe = 'images/buyme.png'
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
-      var place = this.props.data.place;
-      var description= this.props.data.description;
-      // var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a target="_blank" title={network.title} href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
+      if (this.props.data) {
+         var name = this.props.data.name;
+         var occupation = this.props.data.occupation;
+         var place = this.props.data.place;
+         var description = this.props.data.description;
+         // var city= this.props.data.address.city;
+         var networks = this.props.data.social.map(function (network) {
+            return <li key={network.name}><a target="_blank" title={network.title} href={network.url}><i className={network.className}></i></a></li>
+         })
+      }
 
-    return (
-      <header id="home">
+      return (
+         <header id="home">
 
-      <nav id="nav-wrap">
+            <nav id="nav-wrap">
 
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+               <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+               <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Academic Info</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Projects & Blogs</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
+               <ul id="nav" className="nav">
+                  <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                  <li><a className="smoothscroll" href="#about">About</a></li>
+                  <li><a className="smoothscroll" href="#resume">Academic Info</a></li>
+                  <li><a className="smoothscroll" href="#portfolio">Projects & Blogs</a></li>
+                  <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+                  <li><a className="smoothscroll" href="#contact">Contact</a></li>
+               </ul>
 
-      </nav>
+            </nav>
 
-      <div className="row banner">
-         <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a <span>{occupation}</span> at <span>{place}</span>. {description}</h3>
-            <hr />
-            <ul className="social">
-               {networks}
-            </ul>
-         </div>
-      </div>
+            <div className="row banner">
+               <div className="banner-text">
+                  <h1 className="responsive-headline">I'm {name}.</h1>
+                  <h3>I'm a <span>{occupation}</span> at <span>{place}</span>. {description}</h3>
+                  <hr />
+                  <ul className="social">
+                     {networks}
+                  </ul>
+                  <img id="BuyCoffee" src={BuyCoffe}></img>
+               </div>
+            </div>
 
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
+            <p className="scrolldown">
+               <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+            </p>
 
-   </header>
-    );
-  }
+         </header>
+      );
+   }
 }
 
 export default Header;
